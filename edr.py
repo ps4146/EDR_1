1b50029dfde6118edebcba3389e6967960beef0f
Prateek_S98 <prateek@tvsmotor.com>
2022-04-29 21:15:15 -0400

    Removed match keyword for python compatibility reasons

--- edr.py
+++ edr.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser(description = "Python script that can retrieve, create or delete Deny List Policies as well as update Deny Policy comments.")
-parser.add_argument("option", type = int, help="select 1 for retrieving, 2 for creating, 3 for deleting or 4 for updating Deny List Policies")
+parser.add_argument("option", type = int, help="Select 1 for retrieving, 2 for creating, 3 for deleting or 4 for updating Deny List Policies")
 parser.add_argument("auth", type = str, help="Please provide Authorization token for accessing the EDR API.")
 parser.add_argument("--id", type= str, help="Provide id for either retrieving, updating or deleting Deny List Policy")
 parser.add_argument("--ip", type= str, help="Provide IP address for retrieving Deny List Policy with specific IP address")
@@ -17,10 +17,13 @@ args = parser.parse_args()
 if arg.auth is None:
 	print("Please provide the Authorization token for accessing the EDR API.")
 
-match args.option:
-case 1:
-case 2:
-case 3:
-case 4:
-case _:
-	print("Please provide a valid option i.e. 1, 2, 3 or 4 as an argument")
+if arg.option == 1:
+	pass
+elif arg.option == 2:
+	pass
+elif arg.option == 3:
+	pass
+elif arg.option == 4:
+	pass
+elif arg.option is None:
+	print("Please provide a valid option i.e. 1, 2, 3 or 4")
